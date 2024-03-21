f3a95548c348433c88cd321b1328133cd334874c
SURENDHIRAN0228 <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-03-21 11:12:58 +0530

    Update classification.controller.js

--- controllers/classification.controller.js
+++ controllers/classification.controller.js
@@ -2,17 +2,23 @@ const XLSX = require("xlsx");
 const Classification = require("../models/classification.model");
 const outputPath = "storage/outputs";
 
-
 exports.import = async (req, res) => {
+
+    //Read the imported file
     const wb = XLSX.readFile(req.file.path);
     const sheets = wb.SheetNames;
 
+    //To check given file is empty or not
     if (sheets.length > 0) {
         const data = XLSX.utils.sheet_to_json(wb.Sheets[sheets[0]]);
+
         const classification = data.map(row => ({
             classification : row["classification"]
         }));
+        
+         // Insert the data into MySQL
         await Classification.bulkCreate(classification);
     }
+
     res.send({ data: "added successfully" });
 };
