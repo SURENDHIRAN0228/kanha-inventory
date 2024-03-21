efa3a83097177291a94dcbd7022e98f0236b22c7
SURENDHIRAN0228 <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-03-21 11:05:38 +0530

    Update asset_classification.route.js

--- routes/asset_classification.route.js
+++ routes/asset_classification.route.js
@@ -1,3 +1,4 @@
+// Import necessary modules
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
@@ -7,11 +8,16 @@ const fs = require('fs')
 const classificationController = require("../controllers/classification.controller");
 const { upload } = require("../middleware/uploader");
 
-router.post("/import", upload("file"), classificationController.import);
+//Route to Importing csv  file
+router.post("/importClassification", upload("file"), classificationController.import);
 
-router.get("/", function(req, res) {
+// Route to fetch data from MySQL database
+router.get("/GetClassification", function(req, res) {
+
+    //Read data from Mysql database
     req.con.query(`SELECT * FROM asset_classifications` , function (error, result) {
         if (error) {
+            // Handle error
             console.log("Error Connecting to DB");
         } else {
             res.send({ status: true, data: result });
@@ -19,7 +25,10 @@ router.get("/", function(req, res) {
     })
 })
 
-router.get("/:id", function(req, res) {
+// Route to fetch single data from MySql database
+router.get("/getClassification/:id", function(req, res) {
+
+    //Read data from Mysql database
     req.con.query(`SELECT * FROM asset_classifications WHERE id='${req.params.id}'`, function (error, result) {
         if (error) {
             console.log("Error Connecting to DB");
@@ -29,20 +38,29 @@ router.get("/:id", function(req, res) {
     });
 });
 
-router.post('/create', function (req, res) {
+// Define a route to handle data insertion
+router.post('/createClassification', function (req, res) {
+
+    // Extract data from the request body
     const data = req.body
+
+    // Insert data into the database
     req.con.query(`INSERT INTO asset_classifications SET classification = '${data.classification}'`, (error, results) => {
         if(results) {
-            res.send({"status":true, "message":"User created successfully"});
+            res.send({"status":true, "message":"Classification created successfully"});
         } else {
             res.send({"status":false, "message":"Error creating user"});
         } 
     })
 });
 
-router.post('/update/:id', function (req, res) {
+// Route to handle updating data
+router.post('/updateClassification/:id', function (req, res) {
+
+    // Assuming you pass the updated data in the request body
     const data = req.body
-    console.log(req.params.id)
+
+    // Request to update data
     req.con.query(`UPDATE asset_classifications SET classification = '${data.classification}' WHERE id= '${req.params.id}'`, (error, results) => {
         if(results) {
             res.send({"status":true, "message":"Updated successfully"});
@@ -52,7 +70,9 @@ router.post('/update/:id', function (req, res) {
     })
 });
 
-router.delete('/delete/:id', function (req, res) {
+// Route to delete data from MySQL database
+router.delete('/deleteClassification/:id', function (req, res) {
+
     req.con.query(`DELETE FROM asset_classifications WHERE id='${req.params.id}'`, (error, results) => {
         if (results) {
             res.send({ status: true, message: "Deleted Successfully" });
@@ -61,6 +81,44 @@ router.delete('/delete/:id', function (req, res) {
         }
     })
 })
+
+// Route to delete multiple data from MySQL database
+router.delete('/deleteClassificationRows', (req, res) => {
+    // Assuming you send an array of IDs to delete in the request body
+    const idsToDelete = req.body.ids;
+     
+    if (!idsToDelete || !Array.isArray(idsToDelete) || idsToDelete.length === 0) {
+      return res.status(400).json({ error: 'Invalid request. Please provide an array of IDs to delete.' });
+    }
+  
+    // Convert array elements to integers (assuming IDs are integers)
+    const parsedIds = idsToDelete.map(id => parseInt(id, 10));
+  
+    // Construct the SQL query
+    const sql = 'DELETE FROM asset_classifications WHERE id IN (?)';
+    const values = parsedIds;
+
+      // Execute the query
+    req.con.query(sql, [values], (err, result) => {
+      if (err) {
+        console.error('Error executing query: ' + err.message);
+        return res.status(500).json({ error: 'Internal server error.' });
+      }
+  
+      // Check if any rows were affected
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ error: 'No rows found for deletion.' });
+      }
+       // Return success response
+      res.json({ message: 'Rows deleted successfully.' });
+    });
+  
+    
+  });
+  
+
+module.exports = router
+
 
 /*router.post('/login', (req, res) => {
     const data = req.body;
@@ -93,5 +151,3 @@ router.get('/logout', (req, res) => {
 })
 
 */
-
-module.exports = router
