8d52e2fc03b17f103c65cbd485834d9a006f725f
SURENDHIRAN0228 <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-03-21 11:12:09 +0530

    Update asset_details.controller.js

--- controllers/asset_details.controller.js
+++ controllers/asset_details.controller.js
@@ -2,28 +2,31 @@ const XLSX = require("xlsx");
 const AssetDetails = require("../models/asset_details.model");
 const outputPath = "storage/outputs";
 
-
 exports.import = async (req, res) => {
+
+    //Read the imported file
     const wb = XLSX.readFile(req.file.path);
     const sheets = wb.SheetNames;
 
+    //To check given file is empty or not
     if (sheets.length > 0) {
         const data = XLSX.utils.sheet_to_json(wb.Sheets[sheets[0]]);
+        
         const assetDetails = data.map(row => ({
-            asset_name: row["asset_name"],
-            asset_code: row["asset_code"],
-            asset_classification: row["asset_classification"],
-            asset_category: row["asset_category"],
-            asset_date_of_inclusion: row["asset_date_of_inclusion"],
-            asset_manufactured_by: row["asset_manufactured_by"],
-            asset_model: row["asset_model"],
-            manufactured_asset_serial_no: row["manufactured_asset_serial_no"],
-            asset_capacity: row["asset_capacity"],
-            vendor_name: row["vendor_name"]
+            assetName: row["assetName"],
+            assetCode: row["assetCode"],
+            assetClassification: row["assetClassification"],
+            assetCategory: row["assetCategory"],
+            assetDateOfInclusion: row["assetDateOfInclusion"],
+            assetManufacturedBy: row["assetManufacturedBy"],
+            assetModel: row["assetModel"],
+            manufacturedAssetSerialNo: row["manufacturedAssetSerialNo"],
+            assetCapacity: row["assetCapacity"],
+            vendorName: row["vendorName"]
         }));
 
-
+         // Insert the data into MySQL
         await AssetDetails.bulkCreate(assetDetails);
     }
-    res.send({ data: "added successfully" });
+    res.send({ data: "Assets added successfully" });
 };
