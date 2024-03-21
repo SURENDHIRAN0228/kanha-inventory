c438876ed7771fd3a5a7d82e280aa5403468c060
SURENDHIRAN0228 <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-03-21 11:07:41 +0530

    Update asset_location.route.js

--- routes/asset_location.route.js
+++ routes/asset_location.route.js
@@ -1,3 +1,4 @@
+// Import necessary modules
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
@@ -7,11 +8,16 @@ const fs = require('fs')
 const locationController = require("../controllers/location.controller");
 const { upload } = require("../middleware/uploader");
 
-router.post("/import", upload("file"), locationController.import);
+//Route to Importing csv  file
+router.post("/importLocation", upload("file"), locationController.import);
 
-router.get("/", function(req, res) {
+// Route to fetch data from MySQL database
+router.get("/getLocation", function(req, res) {
+
+    //Read data from Mysql database
     req.con.query(`SELECT * FROM asset_locations` , function (error, result) {
         if (error) {
+            // Handle error
             console.log("Error Connecting to DB");
         } else {
             res.send({ status: true, data: result });
@@ -19,7 +25,10 @@ router.get("/", function(req, res) {
     })
 })
 
-router.get("/:id", function(req, res) {
+// Route to fetch single data from MySql database
+router.get("/getLocation/:id", function(req, res) {
+
+    //Read data from Mysql database
     req.con.query(`SELECT * FROM asset_locations WHERE id='${req.params.id}'`, function (error, result) {
         if (error) {
             console.log("Error Connecting to DB");
@@ -29,15 +38,19 @@ router.get("/:id", function(req, res) {
     });
 });
 
-router.post('/create', function (req, res) {
+// Define a route to handle data insertion
+router.post('/createLocation', function (req, res) {
+    
+    // Extract data from the request body
     const data = req.body
-    req.con.query(`INSERT INTO asset_locations SET location_name = '${data.location_name}', department_name = '${data.department_name}', location_type = '${data.location_type}', location_group = '${data.location_group}'`, (error, results) => {
+    
+    // Insert data into the database
+    req.con.query(`INSERT INTO asset_locations SET locationName = '${data.locationName}', departmentName = '${data.departmentName}', locationType = '${data.locationType}', locationGroup = '${data.locationGroup}'`, (error, results) => {
         if(results) {
-            req.con.query(`SELECT MAX(id) AS max_id FROM asset_locations ` , function (error, result, fields) { 
-                const MaxId = result[0].max_id
-                var location_code = "KANLOC00"+MaxId
-                console.log(location_code)
-                req.con.query(`UPDATE asset_locations SET location_code = '${location_code}' WHERE id = '${MaxId}'`, (error, results) => {
+            req.con.query(`SELECT MAX(id) AS maxId FROM asset_locations ` , function (error, result, fields) { 
+                const maxId = result[0].maxId
+                var locationCode = "KANLOC00"+maxId
+                req.con.query(`UPDATE asset_locations SET locationCode = '${locationCode}' WHERE id = '${maxId}'`, (error, results) => {
                     res.send({"status":true, "message":"Location Created Successfully"});
                 })
             })        
@@ -47,10 +60,14 @@ router.post('/create', function (req, res) {
     })
 });
 
-router.post('/update/:id', function (req, res) {
+// Route to handle updating data
+router.post('/updateLocation/:id', function (req, res) {
+
+     // Assuming you pass the updated data in the request body
     const data = req.body
-    console.log(req.params.id)
-    req.con.query(`UPDATE asset_locations SET location_name = '${data.location_name}', department_name = '${data.department_name}', location_type = '${data.location_type}', location_group = '${data.location_group}' WHERE id= '${req.params.id}'`, (error, results) => {
+
+    // Request to update data
+    req.con.query(`UPDATE asset_locations SET locationName = '${data.locationName}', departmentName = '${data.departmentName}', locationType = '${data.locationType}', locationGroup = '${data.locationGroup}' WHERE id= '${req.params.id}'`, (error, results) => {
         if(results) {
             res.send({"status":true, "message":"Updated successfully"});
         } else {
@@ -59,7 +76,8 @@ router.post('/update/:id', function (req, res) {
     })
 });
 
-router.delete('/delete/:id', function (req, res) {
+// Route to delete data from MySQL database
+router.delete('/deleteLocation/:id', function (req, res) {
     req.con.query(`DELETE FROM asset_locations WHERE id='${req.params.id}'`, (error, results) => {
         if (results) {
             res.send({ status: true, message: "Deleted Successfully" });
@@ -69,36 +87,35 @@ router.delete('/delete/:id', function (req, res) {
     })
 })
 
-/*router.post('/login', (req, res) => {
-    const data = req.body;
-    //console.log(data);
-
-    if (data) {
-        req.con.query(`SELECT * FROM students WHERE email = '${data.email}' AND password = '${data.password}'`, (error, results) => {
-           req.con.query(`SELECT id FROM students WHERE email = '${data.email}' `, (error, row) => {
-        
-            if (results.length > 0) {
-                //console.log(row);
-                req.session.loggedin = true;
-                req.session.email = data.email;
-                //res.redirect('/data');
-                res.send({ "status": true, "message": row});
-            } else {
-                res.send({ "status": false, "message": 'Incorrect Username and/or Password!' });
-            }
-        })
-    })
-    } else {
-        res.send('Please enter Username and Password!');
-        res.end();
+// Route to delete multiple data from MySQL database
+router.delete('/deleteLocationRows', (req, res) => {
+    // Assuming you send an array of IDs to delete in the request body
+    const idsToDelete = req.body.ids;
+     
+    if (!idsToDelete || !Array.isArray(idsToDelete) || idsToDelete.length === 0) {
+      return res.status(400).json({ error: 'Invalid request. Please provide an array of IDs to delete.' });
     }
-})
-
-router.get('/logout', (req, res) => {
-    req.session.loggedin = false;
-    res.redirect('/');
-})
-
-*/
+  
+    // Convert array elements to integers (assuming IDs are integers)
+    const parsedIds = idsToDelete.map(id => parseInt(id, 10));
+  
+    // Construct the SQL query
+    const sql = 'DELETE FROM asset_locations WHERE id IN (?)';
+    const values = parsedIds;
+      // Execute the query
+    req.con.query(sql, [values], (err, result) => {
+      if (err) {
+        console.error('Error executing query: ' + err.message);
+        return res.status(500).json({ error: 'Internal server error.' });
+      }
+  
+      // Check if any rows were affected
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ error: 'No rows found for deletion.' });
+      }
+       // Return success response
+      res.json({ message: 'Rows deleted successfully.' });
+    });  
+});
 
 module.exports = router
