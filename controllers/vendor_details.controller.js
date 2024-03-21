4f3d22ff98c167451025a052c8d93094aa9181be
SURENDHIRAN0228 <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-03-21 11:14:35 +0530

    Update vendor_details.controller.js

--- controllers/vendor_details.controller.js
+++ controllers/vendor_details.controller.js
@@ -2,22 +2,29 @@ const XLSX = require("xlsx");
 const Vendor = require("../models/vendor_details.model");
 const outputPath = "storage/outputs";
 
-
 exports.import = async (req, res) => {
+
+    //Read the imported file
     const wb = XLSX.readFile(req.file.path);
     const sheets = wb.SheetNames;
+
+    //To check given file is empty or not
     if (sheets.length > 0) {
         const data = XLSX.utils.sheet_to_json(wb.Sheets[sheets[0]]);
+
         const vendor = data.map(row => ({
-            vendor_shop_name: row["vendor_shop_name"],
-            vendor_code: row["vendor_code"],
-            shop_address: row["shop_address"],
-            contact_no: row["contact_no"],
-            vendor_email: row["vendor_email"],
-            vendor_poc_name: row["vendor_poc_name"],
-            vendor_poc_contact_no: row["vendor_poc_contact_no"]
+            vendorShopName: row["vendorShopName"],
+            vendorCode: row["vendorCode"],
+            shopAddress: row["shopAddress"],
+            contactNo: row["contactNo"],
+            vendorEmail: row["vendorEmail"],
+            vendorPOCName: row["vendorPOCName"],
+            vendorPOCContactNo: row["vendorPOCContactNo"]
         }));
+
+        // Insert the data into MySQL
         await Vendor.bulkCreate(vendor);
     }
-    res.send({ data: "added successfully" });
+    
+    res.send({ data: "Vendors added successfully" });
 };
