e470497015b187076adde6459e1e307b875f582c
SURENDHIRAN0228 <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-03-21 11:14:12 +0530

    Update location_group.controller.js

--- controllers/location_group.controller.js
+++ controllers/location_group.controller.js
@@ -2,18 +2,24 @@ const XLSX = require("xlsx");
 const LocationGroup = require("../models/location_group.models");
 const outputPath = "storage/outputs";
 
-
 exports.import = async (req, res) => {
+
+    //Read the imported file
     const wb = XLSX.readFile(req.file.path);
     const sheets = wb.SheetNames;
 
+    //To check given file is empty or not
     if (sheets.length > 0) {
         const data = XLSX.utils.sheet_to_json(wb.Sheets[sheets[0]]);
+
         const locationGroup = data.map(row => ({
-            location_group : row["location_group"],
-            location_group_under_location : row["location_group_under_location"]
+            locationGroup : row["locationGroup"],
+            locationGroupUnderLocation : row["locationGroupUnderLocation"]
         }));
+
+         // Insert the data into MySQL
         await LocationGroup.bulkCreate(locationGroup);
     }
+
     res.send({ data: "added successfully" });
 };
