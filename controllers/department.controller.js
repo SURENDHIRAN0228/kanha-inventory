f0c69fd657d6759dfbe06a391d494eaca479135b
SURENDHIRAN0228 <73423529+SURENDHIRAN0228@users.noreply.github.com>
2024-03-21 11:13:26 +0530

    Update department.controller.js

--- controllers/department.controller.js
+++ controllers/department.controller.js
@@ -2,22 +2,28 @@ const XLSX = require("xlsx");
 const Department = require("../models/department.model");
 const outputPath = "storage/outputs";
 
-
 exports.import = async (req, res) => {
+
+    //Read the imported file
     const wb = XLSX.readFile(req.file.path);
     const sheets = wb.SheetNames;
 
+    //To check given file is empty or not
     if (sheets.length > 0) {
         const data = XLSX.utils.sheet_to_json(wb.Sheets[sheets[0]]);
+
         const department = data.map(row => ({
-            department_name: row["department_name"],
-            department_code: row["department_code"],
-            department_email: row["department_email"],
-            department_hod_name: row["department_hod_name"],
-            department_hod_email: row["department_hod_email"],
-            department_hod_phone: row["department_hod_phone"]
+            departmentName: row["departmentName"],
+            departmentCode: row["departmentCode"],
+            departmentEmail: row["departmentEmail"],
+            departmentHODName: row["departmentHODName"],
+            departmentHODEmail: row["departmentHODEmail"],
+            departmentHODPhone: row["departmentHODPhone"]
         }));
+
+         // Insert the data into MySQL
         await Department.bulkCreate(department);
     }
-    res.send({ data: "added successfully" });
+
+    res.send({ data: "File Imported! & Departments Added Successfully" });
 };
